575a13ff1ad3ab42333b1f2df6f3cf91eaf82455
Lyeng22 <lyeng2210@gmail.com>
2023-09-24 02:03:20 +0700

    responsive

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 
 export const CreateTodo = ({onCreateTodo}) => {
     const [todo,setTodo] = useState('');
+    const [data,setData] = useState(false);
 
     const handleChange = (event) => {
         setTodo(event.target.value);
+    }
+
+    const handleDataConfirm = () => {
+        setData(false);
     }
 
     const handleSubmit = (event) => {
@@ -14,17 +19,45 @@ export const CreateTodo = ({onCreateTodo}) => {
             onCreateTodo(todo);
             setTodo('')
         }
+        else{
+            setData(true);
+        }
 
     }
 
     return (
+    <>
+        {/* check data */}
+        { data && (
+            <div className="modal-container">
+                <div className="confirm-box">
+                    <div className="confirm-header">
+                        <div className="confirm-content">
+                            <h5 className="confirm-title">Message Comfirmation</h5>
+                        </div>
+                        <div className="confirm-body">
+                            <p>There no data for adding</p>
+                        </div>
+                        <div className="confirm-footer">
+                            <button className="confirm-button" onClick={handleDataConfirm}>
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        )}
+
         <div onSubmit={handleSubmit}>
             <form className="todo-form">
                 <input className="todo-input" placeholder="Write your task here"
-                 value={todo} onChange={handleChange}
-                  />
+                value={todo} onChange={handleChange}
+                />
                 <button className="add-button">Add</button>
             </form>
         </div>
+
+    </>
     );
 }
